93675f66fbf9c69c87b2b7a4e97e7654310760ca
hoodarohit <hooda.r@northeastern.edu>
2023-05-19 12:11:32 -0400

    Made the website responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 function About() {
     return (
-        <div name="about"
-             className='display-flex flex-col justify-center items-center w-full h-screen bg-gradient-to-b from-gray-800 to-black w-max-content mx-auto my-auto h-max-content text-white'>
-
+        <div name="about" className='flex flex-col justify-center items-center w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
@@ -12,34 +10,27 @@ function About() {
                     </p>
                 </div>
                 <div>
-                    <p className='text-xl mt-20'>
-                        Passionate about Software development and currently a graduate student at
-                        Northeastern University.
-                        After the completion of my Bachelor from GTU Gujarat,
-                        India worked for TechNeos Solutions,Ahmedabad, India for 6 months as a
-                        Software Developer Intern.
-                        I have a track record of developing code and digital solutions using
-                        cutting-edge technologies for startups.
-                        Other than that was also responsible for support where it needed to resolve
-                        defects in real-time.
+                    <p className='text-xl mt-10 lg:mt-20'>
+                        Passionate about Software development and currently a graduate student at Northeastern University. After the completion of my Bachelor from GTU Gujarat, India, I worked for TechNeos Solutions, Ahmedabad, India for 6 months as a Software Developer Intern. I have a track record of developing code and digital solutions using cutting-edge technologies for startups. Other than that, I was also responsible for support where it needed to resolve defects in real-time.
                     </p>
                     <br/>
                     <p className='text-xl w-120'>
                         My expertise lies in the following:
                         <br/>
 
-                        <b className='text-blue-500'>Programming Languages:</b> <span> Java, JavaScript, Typescript, Python, Object-Oriented Design</span>
+                        <b className='text-blue-500'>Programming Languages:</b> <span>Java, JavaScript, Typescript, Python, Object-Oriented Design</span>
                         <br/>
-                        <b className='text-blue-500'>Databases:</b> <span> MySQL, MongoDB, PostgreSQL </span>
+                        <b className='text-blue-500'>Databases:</b> <span>MySQL, MongoDB, PostgreSQL</span>
                         <br/>
                         <b className='text-blue-500'>Technologies:</b> <span>HTML5, CSS3, Bootstrap5, Hibernate, Spring, Spring-boot, Express, NodeJS, ReactJS, Redux, JSON, RESTful APIs, Swing, XML</span>
                         <br/>
-                        <b className='text-blue-500'>Software/Tools:</b> <span> Postman, IntelliJ, Tomcat, VS Code, Jira, DevOps, Github Actions, CI/CD, Docker, ETL, TDD, Maven, Git, Junit</span><br/>
-                        <b className='text-blue-500'>Cloud Technology: </b><span> EC2, S3, CloudFront, RDS, Cloudwatch, AWS Lambda, CloudFront, IAM, Metrics, AMI , User Data, Terraform, Packer</span><br/>
+                        <b className='text-blue-500'>Software/Tools:</b> <span>Postman, IntelliJ, Tomcat, VS Code, Jira, DevOps, Github Actions, CI/CD, Docker, ETL, TDD, Maven, Git, Junit</span><br/>
+                        <b className='text-blue-500'>Cloud Technology:</b> <span>EC2, S3, CloudFront, RDS, Cloudwatch, AWS Lambda, CloudFront, IAM, Metrics, AMI, User Data, Terraform, Packer</span><br/>
                     </p>
                 </div>
             </div>
         </div>
+
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,25 @@ import {Link} from "react-scroll";
 const Home = () => {
 
     const handleDemoUrl = () => {
-        window.open("https://drive.google.com/file/d/12J7eVN49ObaMljBLds09sWzy84ApSHew/view?usp=sharing", '_blank')
+        window.open(
+            "https://drive.google.com/file/d/12J7eVN49ObaMljBLds09sWzy84ApSHew/view?usp=sharing",
+            '_blank')
     }
 
     return (
-        <div name="home" className="display-flex flex-col justify-center h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
-            <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
+        <div name="home"
+             className="flex flex-col justify-center min-h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+            <div
+                className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
                 <div className="flex flex-col justify-center h-full md:w-1/2">
-                    <h2 className="text-white text-4xl sm:text-7xl font-bold md:text-6xl">
+                    <h2 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
                         I'm a Full Stack Developer
                     </h2>
                     <p className="text-gray-500 py-4 max-w-md text-lg">
-                        I'm a passionate developer who is always looking for new challenges. I love learning new things and building things.
-                        I have Experience in MERN stack, Java Frameworks, Frontend/Backend/Cloud Technologies and DevOps
+                        I'm a passionate developer who is always looking for new challenges. I love
+                        learning new things and building things.
+                        I have Experience in MERN stack, Java Frameworks, Frontend/Backend/Cloud
+                        Technologies, and DevOps
                     </p>
                     <div>
                         <Link
@@ -29,31 +35,33 @@ const Home = () => {
                         >
                             Portfolio
                             <span className="group-hover:rotate-90 duration-300">
-                                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-                            </span>
+            <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
+          </span>
                         </Link>
                         <span>
-                            <button
-                                onClick={() => handleDemoUrl()}
-                                className="bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:text-black text-white py-2 px-4 rounded inline-flex items-center"
-                            >
-                                <svg
-                                    className="fill-current w-9 h-6 mr-2"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                                </svg>
-                                <span>Resume</span>
-                            </button>
-                        </span>
+          <button
+              onClick={() => handleDemoUrl()}
+              className="bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:text-black text-white py-2 px-4 rounded inline-flex items-center"
+          >
+            <svg
+                className="fill-current w-9 h-6 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+            >
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+            </svg>
+            <span>Resume</span>
+          </button>
+        </span>
                     </div>
                 </div>
                 <div>
-                    <img src={HeroImage} alt="My profile" className="rounded-2xl mx-auto w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2" />
+                    <img src={HeroImage} alt="My profile"
+                         className="rounded-2xl mx-auto w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2"/>
                 </div>
             </div>
         </div>
+
     )
 }
 
